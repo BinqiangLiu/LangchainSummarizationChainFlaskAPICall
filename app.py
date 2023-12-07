d2906197185b889466c5ad322a63ed032284a5b2
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-12-07 11:21:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 import streamlit as st
-
+import json
 
 st.set_page_config(page_title="Webpage Summarization AI Assistant - Open Source Version", layout="wide")
 st.subheader("Webpage Summarization AI Assistant - Open Source Version")
@@ -10,18 +10,20 @@ css_file = "main.css"
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
-def call_chatbot_api(query):
+def call_chatbot_api(target_url):
     #url = 'https://binqiangliu-flask-inference-api.hf.space/api/chat'
     url = 'https://ishare-langchainsummarizationchainflaskapi.hf.space/api/chat'
-    json_data_for_api = {'user_question': query}
-    response = requests.post(url, json=json_data_for_api) 
+    json_data_for_api = {'target_url': target_url}
+    #response = requests.post(url, json=json_data_for_api)
+    response = requests.post(url, headers=headers, data=json.dumps(json_data_for_api))   #This format needs 'import json', or else NameError: name 'json' is not defined
+
     result = response.json()
     return result['response']
     
-user_query = st.text_input("Enter the URL of the webpage your want to summarize:")
+target_url = st.text_input("Enter the URL of the webpage your want to summarize:")
 with st.spinner("AI Thinking...Please wait a while to Cheers!"):    
-    if user_query !="" and not user_query.strip().isspace() and not user_query == "" and not user_query.strip() == "" and not user_query.isspace():
-        response = call_chatbot_api(user_query)
+    if target_url !="" and not target_url.strip().isspace() and not target_url == "" and not target_url.strip() == "" and not target_url.isspace():
+        response = call_chatbot_api(target_url)
         st.write("AI Response:")
         st.write(response)
         print(response)  # 打印Chatbot的响应
